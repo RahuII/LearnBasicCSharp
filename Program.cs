f2bc71d7b1ef4cf4dddb5f24bbe61155eae0c745
divyansh chaudhary <cdivyansh470@gmail.com>
2023-02-13 11:35:46 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+   public class Program
+{
+    static void Main(string[] args)
+    {
+        Employee[] employees = new Employee[3];
+        employees[0] = new Employee();
+        employees[1] = new PartTimeEmployee();
+        employees[2] = new FullTime();
+
+        foreach (Employee e in employees)
+        {
+            e.PrintFullName();
+        }
+    }
+}
+public class Employee
+{
+    public string FirstName = "Rahul ";
+    public string lastName = "Kumar";
+
+    public virtual void PrintFullName() { 
+        Console.WriteLine(FirstName+lastName);
+    }
+}
+public class PartTimeEmployee : Employee
+{
+    public override void PrintFullName()
+    {
+        Console.WriteLine(FirstName + lastName+" Part Time");
+    }
+}
+public class FullTime : Employee
+{
+    public override void PrintFullName()
+    {
+        Console.WriteLine(FirstName + lastName+ " Full time");
+    }
+}
+
