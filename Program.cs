e5347841f7e6dc6f376ec3f25478684705072c08
Rahul Kumar <r1303yadav@gmail.com>
2023-02-15 11:21:15 +0530

    creating indexers

--- Program.cs
+++ Program.cs
@@ -1,29 +1,42 @@
 ﻿namespace LearnBasic
 {
     public delegate void SimpleDelegate();
-    class Program
+
+    public class Employee
     {
-        public static void Main()
-        {
-            SimpleDelegate del1, del2, del3;
-            del1 = new SimpleDelegate(simpleMethodOne);
-            del2 = new SimpleDelegate(simpleMethodTwo);
-            del3 = new SimpleDelegate(simpleMethodThree);
-        }
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
 
+    }
 
-        public static void simpleMethodOne()
+    public class Company
+    {
+        private List<Employee> listEmployees;
+        public Company()
         {
-            Console.WriteLine("SimpleMethodOne invoked ");
-        }
-        public static void simpleMethodTwo()
-        {
-            Console.WriteLine("SimpleMethodTwo invoked ");
-        }
-        public static void simpleMethodThree()
-        {
-            Console.WriteLine("SimpleMethodThree invoked ");
-        }
+            listEmployees = new List<Employee>();
 
+            listEmployees.Add(new Employee { EmployeeId = 1, Name = "Mike", Gender = "Male" });
+            listEmployees.Add(new Employee { EmployeeId = 2, Name = "Pam", Gender = "Female" });
+            listEmployees.Add(new Employee { EmployeeId = 3, Name = "John", Gender = "Male" });
+            listEmployees.Add(new Employee { EmployeeId = 4, Name = "Maxine", Gender = "Female" });
+            listEmployees.Add(new Employee { EmployeeId = 5, Name = "Emiliy", Gender = "Female" });
+            listEmployees.Add(new Employee { EmployeeId = 6, Name = "Scott", Gender = "Male" });
+            listEmployees.Add(new Employee { EmployeeId = 7, Name = "Todd", Gender = "Male" });
+            listEmployees.Add(new Employee { EmployeeId = 8, Name = "Ben", Gender = "Male" });
+        }
+        public string this[int employeeId]
+        {
+            // Just like properties indexers have get and set accessors
+            get
+            {
+                return listEmployees.FirstOrDefault(x => x.EmployeeId == employeeId).Name;
+            }
+            set
+            {
+                listEmployees.FirstOrDefault(x => x.EmployeeId == employeeId).Name = value;
+            }
+        }
     }
 }
