6af5ced2defbb506a49caeaa9376be6fa39aa3ab
Rahul Kumar <r1303yadav@gmail.com>
2023-02-15 10:09:39 +0530

    delegates complete

--- Program.cs
+++ Program.cs
@@ -1,35 +1,28 @@
 ﻿namespace LearnBasic
 {
+    public delegate void SimpleDelegate();
     class Program
     {
         public static void Main()
         {
-            List<Employee> empList = new List<Employee>();
-            empList.Add(new Employee() { ID = 101, Name = "Mary", Salary = 5000, Experience = 5 });
-            empList.Add(new Employee() { ID = 101, Name = "Mike", Salary = 4000, Experience = 4 });
-            empList.Add(new Employee() { ID = 101, Name = "John", Salary = 6000, Experience = 6 });
-            empList.Add(new Employee() { ID = 101, Name = "Todd", Salary = 3000, Experience = 3 });
-            Employee.PromotEmployee(empList, emp => emp.Experience >= 5);
+            SimpleDelegate del1, del2, del3;
+            del1 = new SimpleDelegate(simpleMethodOne);
+            del2 = new SimpleDelegate(simpleMethodTwo);
+            del3 = new SimpleDelegate(simpleMethodThree);
         }
-    }
-    delegate bool IsPromotable(Employee empl);
 
-    class Employee
-    {
-        public int ID { get; set; }
-        public string Name { get; set; }
-        public int Salary { get; set; }
-        public int Experience { get; set; }
 
-        public static void PromotEmployee(List<Employee> employeesList, IsPromotable IsEligibleToPromote)
+        public static void simpleMethodOne()
         {
-            foreach (Employee employee in employeesList)
-            {
-                if (employee.Experience >= 5)
-                {
-                    System.Console.WriteLine(employee.Name + " promoted");
-                }
-            }
+            Console.WriteLine("SimpleMethodOne invoked ");
+        }
+        public static void simpleMethodTwo()
+        {
+            Console.WriteLine("SimpleMethodTwo invoked ");
+        }
+        public static void simpleMethodThree()
+        {
+            Console.WriteLine("SimpleMethodThree invoked ");
         }
 
     }
