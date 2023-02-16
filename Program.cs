81e4c82fea73f14d2efee87953acab8faef20f7b
Rahul Kumar <r1303yadav@gmail.com>
2023-02-16 12:54:58 +0530

    play with xml using linq

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Runtime.InteropServices;
+using System.Xml.Linq;
 
 namespace LearnBasic
 {
@@ -6,15 +7,39 @@ namespace LearnBasic
     {
         static void Main(string[] args)
         {
-            int[] numbers = new int[] { 67, 45, 4, 64, 45 };
+            string studentsXML = @"<Students>
+                                    <Student> 
+                                        <Name>Rahul></Name>
+                                        <Age>21</Age>
+                                        <University>Marwadi</University>
+                                    </Student>
+                                    <Student> 
+                                        <Name>deep></Name>
+                                        <Age>41</Age>
+                                        <University>GTU</University>
+                                    </Student>
+                                    <Student> 
+                                        <Name>Vipul></Name>
+                                        <Age>65</Age>
+                                        <University>Marwadi</University>
+                                    </Student>
+                                </Students>";
+            XDocument studentsXdoc = new XDocument();
+            studentsXdoc = XDocument.Parse(studentsXML);
+            var students = from student in studentsXdoc.Descendants("Student")
+                           select new
+                           {
+                               Name = student.Element("Name").Value,
+                               Age = student.Element("Age").Value,
+                               University = student.Element("University").Value,
+                           };
 
-            var query = from number in numbers where number > 5 orderby number descending select number;
-
-            foreach (var i in query)
+            foreach (var student in students)
             {
-                System.Console.WriteLine(i);
+                System.Console.WriteLine("Student {0} with age {1} from University {2}", student.Name, student.Age, student.University);
             }
+
         }
     }
-    
+
 }
